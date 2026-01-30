4f113aae4082b6011e2fa4624ec09c3010085fb6
antonidev0 <antonibogado0@gmail.com>
2026-01-30 11:42:40 +0100

    Agrego la logica de aplicar en el boton apply-button.js

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,23 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+
+// Se selecciona el boton para aplicar
+const jobListingSection = document.querySelector('.jobs-istings');
+
+// escucho cualquier click que ocurra 
+jobListingSection.addEventListener('click', function
+    (event) {
+    
+    // obtengo el elemento button que fue clickeado
+    console.log(event)
+    const element = event.target
+
+    // verifico que tenga la clase 
+    if (element.classList.contains('button-apply-job')) {
+        // cambio el texto
+        element.textContent = '¡Aplicado!'
+        // le agrego la clase is applied
+        element.classList.add('is-applied')
+        // desbilito el boton 
+        element.disable = true
+    }
+    })
