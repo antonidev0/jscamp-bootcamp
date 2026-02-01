a3516c62107665dcc4dc385aa3087d9f1e041083
antonidev0 <antonibogado0@gmail.com>
2026-02-01 21:31:17 +0100

    agrego filtro por tecnologia

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -2,6 +2,9 @@
 
 // selecciono el filtro de lacacion del elemento slect
 const filter = document.querySelector("#filter-location");
+// seleeciono el filtro por tecnologia
+const filterTech = document.querySelector("#filter-technology");
+// selecciono filtro por nivel de experiencia
 const filterExp = document.querySelector("#filter-experience-level");
 // selecciono el mensaje de texto donde se muestra el valor seleccionado
 const mensaje = document.querySelector("#filter-selected-value");
@@ -35,6 +38,37 @@ filter.addEventListener("change", function () {
     job.classList.toggle("is-hidden", isShown === false);
   });
 });
+
+// evento para filtrar por nivel
+filterTech.addEventListener("change", function () {
+  // selecciono todas las tarjetas de trabajo
+  const jobs = document.querySelectorAll(".job-listing-card");
+
+  // obtengo el valor seleccionado
+  const selectedValue = filterTech.value;
+
+  // si hay un valor seleccionado, muestro el mensaje
+  if (selectedValue) {
+    mensaje.textContent = `Has seleccionado ${selectedValue}`;
+  } else {
+    // si no hay valor seleccionado, limpio el mensaje
+    mensaje.textContent = "";
+  }
+
+  // recorro todas las tarjetas de trabajo
+  jobs.forEach((job) => {
+    // obtengo la experiencia de la tarjeta
+    const tech = job.getAttribute("data-technology");
+
+    // si no hay filtro seleccionado muestro todas las tarjetas
+    // si hay filtro seleccionado muestro solo las que coinciden
+    const isShown = selectedValue === "" || selectedValue === tech;
+
+    // y muestro la tarjeta segun corresponda
+    job.classList.toggle("is-hidden", isShown === false);
+  });
+});
+
 
 // evento para filtrar por experiencia
 filterExp.addEventListener("change", function () {
