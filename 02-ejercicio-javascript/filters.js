1094a85740d7849d6b81f873436009baf05435d3
antonidev0 <antonibogado0@gmail.com>
2026-02-01 21:32:32 +0100

    ordeno

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -9,37 +9,8 @@ const filterExp = document.querySelector("#filter-experience-level");
 // selecciono el mensaje de texto donde se muestra el valor seleccionado
 const mensaje = document.querySelector("#filter-selected-value");
 
-// escucho el evento change sobre el select
-filter.addEventListener("change", function () {
-  // selecciono todas las tarjetas de trabajo
-  const jobs = document.querySelectorAll(".job-listing-card");
 
-  // obtengo el valor seleccionado
-  const selectedValue = filter.value;
-
-  // si hay un valor seleccionado, muestro el mensaje
-  if (selectedValue) {
-    mensaje.textContent = `Has seleccionado ${selectedValue}`;
-  } else {
-    // si no hay valor seleccionado, limpio el mensaje
-    mensaje.textContent = "";
-  }
-
-  // recorro todas las tarjetas de trabajo
-  jobs.forEach((job) => {
-    // obtengo la modalidad de la tarjeta
-    const modalidad = job.getAttribute("data-modalidad");
-
-    // si no hay filtro seleccionado muestro todas las tarjetas
-    // si hay filtro seleccionado muestro solo las que coinciden
-    const isShown = selectedValue === "" || selectedValue === modalidad;
-
-    // y muestro la tarjeta segun corresponda
-    job.classList.toggle("is-hidden", isShown === false);
-  });
-});
-
-// evento para filtrar por nivel
+// evento para filtrar por tecnologia
 filterTech.addEventListener("change", function () {
   // selecciono todas las tarjetas de trabajo
   const jobs = document.querySelectorAll(".job-listing-card");
@@ -69,6 +40,35 @@ filterTech.addEventListener("change", function () {
   });
 });
 
+// escucho el evento change sobre el select
+filter.addEventListener("change", function () {
+  // selecciono todas las tarjetas de trabajo
+  const jobs = document.querySelectorAll(".job-listing-card");
+
+  // obtengo el valor seleccionado
+  const selectedValue = filter.value;
+
+  // si hay un valor seleccionado, muestro el mensaje
+  if (selectedValue) {
+    mensaje.textContent = `Has seleccionado ${selectedValue}`;
+  } else {
+    // si no hay valor seleccionado, limpio el mensaje
+    mensaje.textContent = "";
+  }
+
+  // recorro todas las tarjetas de trabajo
+  jobs.forEach((job) => {
+    // obtengo la modalidad de la tarjeta
+    const modalidad = job.getAttribute("data-modalidad");
+
+    // si no hay filtro seleccionado muestro todas las tarjetas
+    // si hay filtro seleccionado muestro solo las que coinciden
+    const isShown = selectedValue === "" || selectedValue === modalidad;
+
+    // y muestro la tarjeta segun corresponda
+    job.classList.toggle("is-hidden", isShown === false);
+  });
+});
 
 // evento para filtrar por experiencia
 filterExp.addEventListener("change", function () {
