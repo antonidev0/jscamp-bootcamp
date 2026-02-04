cbbe5999476ff18cffd801dcf45161d54f179f88
antonidev0 <antonibogado0@gmail.com>
2026-02-04 23:02:01 +0100

    pruebas

--- 03-ejercicio-react/src/main.jsx
+++ 03-ejercicio-react/src/main.jsx
@@ -1,8 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { JobCard } from './pages/ejemplo.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <JobCard titulo="soy un titulo" empresa="google" />,
-);
+createRoot(document.getElementById('root')).render(
+  <App />
+)
