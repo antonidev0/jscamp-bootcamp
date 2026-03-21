1c6a9d692243a8637946aab105f9726b1b70c77f
antonidev0 <antonibogado0@gmail.com>
2026-03-21 01:38:32 +0100

    mantengo la URL del navegador sincronizado con los filtros y elimino el loop infinito

--- 04-ejercicio-react-router-y-estado-global/src/context/FiltersContext.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/context/FiltersContext.jsx
@@ -104,25 +104,18 @@ export function FilterProvider({ children }) {
   }, [filters, textToFilter, currentPage, retryCount]);
 
   useEffect(() => {
-    setSearchParams((params) => {
-      textToFilter ? params.set("text", textToFilter) : params.delete("text");
-      filters.technology
-        ? params.set("technology", filters.technology)
-        : params.delete("technology");
-      filters.location
-        ? params.set("location", filters.location)
-        : params.delete("location");
-      filters.experience
-        ? params.set("experience", filters.experience)
-        : params.delete("experience");
-      currentPage > 1
-        ? params.set("page", String(currentPage))
-        : params.delete("page");
+    const params = new URLSearchParams();
+      if (textToFilter) params.set("text", textToFilter);
+      if (filters.technology) params.set("technology", filters.technology);
+      if (filters.location) params.set("location", filters.location);
+      if (filters.experience) params.set("experience", filters.experience);
+      if (currentPage > 1) params.set("page", String(currentPage));
 
-      return params;
-    });
-  }, [filters, textToFilter, currentPage, navigateTo]);
+      setSearchParams(params, { replace: true });
+    
+  }, [filters, textToFilter, currentPage]);
 
+  
   const jobsFilteredByFields = useMemo(
     () => {
       return jobsData.filter((job) => {
