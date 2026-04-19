9453d59eb7c86274b1f31ddecbb27947950d2260
antonidev0 <antonibogado0@gmail.com>
2026-04-19 23:03:58 +0200

    Agrego las paginas Register, Login, y sus estilos, ademas el ProtectedRoute para las proteger las rutas como el profile

--- 04-ejercicio-react-router-y-estado-global/src/App.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import { lazy, Suspense, useEffect, useState } from "react";   
 import { useRouter } from "./hooks/useRouter.jsx";  
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 
 const HomePage = lazy(() => import("../src/pages/Home.jsx"));
 const Header = lazy(() => import("../src/components/Header.jsx"));
@@ -9,6 +10,9 @@ const SearchPage = lazy(() => import("../src/pages/Search.jsx"));
 const NotFound = lazy(() => import("./pages/404.jsx"));
 const Contact = lazy(() => import("./pages/Contact.jsx"));
 const JobDetail = lazy(() => import("./pages/Detail.jsx"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage.jsx"));
+const LoginPage = lazy(() => import("./pages/Login.jsx"));
+const RegisterPage = lazy(() => import("./pages/Register.jsx"));
 
 // Componente principal que provee el contexto
 
@@ -16,14 +20,23 @@ function App() {
 
   return (
     <>
-      <Header/>
-        <Routes>
-          <Route path="/" element={<HomePage/>} exact={false} />
-          <Route path="/search" element={<SearchPage/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/jobs/:jobId" element={<JobDetail/>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} exact={false} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/jobs/:jobId" element={<JobDetail />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute redirectTo="/login">
+              <ProfilePage />
+            </ProtectedRoute>
+          } />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   );
