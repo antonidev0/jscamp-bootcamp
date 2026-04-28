a3d56d1bdbc55feb745d1d27c5603e2411af6db1
antonidev0 <antonibogado0@gmail.com>
2026-04-28 06:20:03 +0200

    pruebas con el server.js

--- 05-ejercicio-cli-con-node/server.js
+++ 05-ejercicio-cli-con-node/server.js
@@ -1,10 +1,28 @@
 import { createServer } from "node:http";
 
+process.loadEnvFile();
+
 const DESIRED_PORT = process.env.PORT ?? 3000;
 
 const server = createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end("¡Hola Mundo! 🚀 Aquí tienes un servidor con tildes.");
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+      const { url } = req;
+
+      if (url === "/") {
+        res.end("<h1>Bienvenido a la Home</h1>");
+      } else if (url === "/usuarios") {
+        res.end("<h1>Lista de usuarios</h1>");
+      } else {
+        res.end("<h1>404 Not Found</h1>");
+    } 
+
+    if (url === "/json") {        
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
+        const user = { name: 'midudev', role: 'admin' }
+        res.end(JSON.stringify(user));
+    } else {
+           res.end("<h1>404 Not Found</h1>");
+    }
 });
 
 server.listen(DESIRED_PORT, () => {
