1e52ef3b92a8addec61dcedf2a5c9224a44e127c
antonidev0 <antonibogado0@gmail.com>
2026-05-20 04:30:38 +0200

    Agrego cambios sugeridos

--- 06-ejercicio-servidor-con-node/server.js
+++ 06-ejercicio-servidor-con-node/server.js
@@ -2,15 +2,13 @@ import { createServer } from "node:http";
 import { json } from "node:stream/consumers";
 import { randomUUID } from "node:crypto";
 import { readFile } from "node:fs/promises"; // lee los archivos nativos y te los da en bruto
+import dataJobs from "./data.json" with { type: "json" }; //mas rapido
 
 process.loadEnvFile();
 
 const DESIRED_PORT = process.env.PORT ?? 3000;
 
-// traeme los datos de data en texto y damelos como un array de obejetos
-  // (lo dejo aca para que cargue una vez y guarde los cambios del post en cada sesion)
-  const users = JSON.parse(await readFile("./data.json", "utf-8"));
-
+// traeme los datos de data en texto y damelos como un array de obejetos  
 
 const server = createServer(async (req, res) => {
   const { method, url } = req; //dentro del req, toma el objeto url y el metodo y guardalo
@@ -19,7 +17,8 @@ const server = createServer(async (req, res) => {
   const parsedUrl = new URL(url, `http://localhost:${DESIRED_PORT}`);
 
   // tomamos los query de la url
-  const pathname = parsedUrl.pathname;
+  // const pathname = parsedUrl.pathname;
+  // no sabia que igual leia el pathname
 
   function sendJson(res, statusCode, data) {
     res.statusCode = statusCode; // oye, el estado de la respuesta, es el estado que te envie
@@ -43,12 +42,12 @@ const server = createServer(async (req, res) => {
       const maxAge = parsedUrl.searchParams.get("maxAge");
 
       // los nombres filtrado van a ser igual a los usuairos
-      let filteredUsers = users;
+      let filteredUsers = dataJobs;
 
       // si en la url existe "name"
       if (nameFilter) {
         // filtramos usuarios cuyo nombre contenga el texto (sin distinguir mayusculas)
-        filteredUsers = users.filter((user) =>
+        filteredUsers = dataJobs.filter((user) =>
           // dame el nombre del filtro de la url en minuscula y si
           //  en el nombre de los usuarios incluye alguna letra de lo que busque , damelo
           user.name.toLowerCase().includes(nameFilter.toLowerCase()),
@@ -57,17 +56,32 @@ const server = createServer(async (req, res) => {
 
       // si en la url existe minAge
       if (minAge) {
+
+        // quiero que la edad minima sea un siempre un numero
+        const minAgeNumber = Number(minAge); 
+        // sera invalido si es menor a cero o si no es un numero 
+        const isInvalidNumber = Number.isNaN(minAgeNumber) || minAgeNumber < 0;
+        
+        // si es un numero invalido 
+        if (isInvalidNumber) return sendJson(res, 400, { error: "minAge debe ser un numero valido" })
+        
         // si la edad del usuario es mayor o igual al minimo dejalo en la list
         filteredUsers = filteredUsers.filter(
-          (user) => user.age >= Number(minAge),
+          (user) => user.age >= Math.floor(minAgeNumber),
         );
       }
 
       // i en la url existe maxAge
       if (maxAge) {
+        
+        // mismas condiciones que en minAge
+        const maxAgeNumber = Number(maxAge)
+        const isInvalidNumber = Number.isNaN(maxAgeNumber) || maxAgeNumber < 0
+        
+        if (isInvalidNumber) return sendJson(res, 400, { error: "maxAge debe ser un numero valido" })
         // si la edad del usuario es menor o igual al maximo dejalo en la lista
         filteredUsers = filteredUsers.filter(
-          (user) => user.age <= Number(maxAge),
+          (user) => user.age <= Math.floor(maxAge),
         );
       }
 
@@ -78,22 +92,45 @@ const server = createServer(async (req, res) => {
         // cantidad de usuarios a saltar desde el inicio es
         const numOffset = Number(offset);
 
-        // dame el nummero limite de los usuarios con la cantidad a saltarse, y separamelo de los usuarios filtrados
-        filteredUsers = filteredUsers.slice(numOffset, numOffset + numLimit);
+        // si es menor a cero o dirente a un numero es invalido
+        const isInvalidLimit = Number.isNaN(numLimit) || numLimit < 0;
+        const isInvalidOffset = Number.isNaN(numOffset) || numOffset < 0;
+        
+        if (isInvalidLimit || isInvalidOffset) {
+          return sendJson(res, 400, { error: "limit y offset deben ser numeros validos" });
+        }
+
+        // dame el nummero limite de los usuarios con la cantidad a saltarse, y
+        //  separamelo de los usuarios filtrados
+        // Evitamos decimales
+        filteredUsers = filteredUsers.slice(Math.floor(numOffset), Math.floor(numOffset + numLimit));
       }
 
       // y luego me envias los usuraios que filtraste
       return sendJson(res, 200, filteredUsers);
-    } else if (parsedUrl.pathname === "/health") {
+    }
+    
+    if (parsedUrl.pathname === "/health") {
       return sendJson(res, 200, { status: "ok", uptime: process.uptime() });
     }
+
+    if (parsedUrl.pathname === "/") {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      // si la url es / muestrae este elemento html
+      res.end("<h1>Bienvenido a la Home</h1>");
+    } else if (url === "/usuarios") {
+      // si es /usuarios muestrame
+      res.end("<h1>Lista de usuarios</h1>");
+    } else {
+      // si no es ninguna de las anterioeres envia este error
+      return sendJson(res, 404, { error: "Ruta no Encontrada" });
+    }
+
   }
 
   if (method === "POST") {
     if (parsedUrl.pathname === "/users") {
-      const body = await json(req);
-      console.log(body, "Aaaaaaaaaa");
-      
+      const body = await json(req); 
 
       if (!body || !body.name || !body.age) {
         return sendJson(res, 400, {
@@ -109,23 +146,13 @@ const server = createServer(async (req, res) => {
       };
       
 
-      users.push(newUser);
+      dataJobs.push(newUser);
 
       return sendJson(res, 201, { message: "usuario creado" });
     }
   }
 
-  if (parsedUrl.pathname === "/") {
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    // si la url es / muestrae este elemento html
-    res.end("<h1>Bienvenido a la Home</h1>");
-  } else if (url === "/usuarios") {
-    // si es /usuarios muestrame
-    res.end("<h1>Lista de usuarios</h1>");
-  } else {
-    // si no es ninguna de las anterioeres envia este error
-    return sendJson(res, 404, { error: "Ruta no Encontrada" });
-  }
+  
 });
 
 server.listen(DESIRED_PORT, () => { 
