c80e6a78855aa90dde1a8395e97ac78faa56ecd4
antonidev0 <antonibogado0@gmail.com>
2026-05-31 15:52:53 +0200

    Agrego avances

--- ejercicio-express/index.js
+++ ejercicio-express/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import jobs from './jobs.json' with { type: 'json' }
+
 
 process.loadEnvFile();
 const PORT_SERVER = process.env.PORT ?? 1234
@@ -17,7 +19,32 @@ app.get("/health", (request, response) => {
   });
 });
 
+app.get('/get-jobs', (req, res) => {
 
+  const { text, title, level, limit, technology, offset } = req.query
+  
+  let filteredJobs = jobs
+
+  if (text) {
+    const searchTerm = text.toLowerCase()
+    filteredJobs = filteredJobs.filter(job =>
+        job.titulo.toLocaleLowerCase().includes(searchTerm) ||
+        job.descripcion.toLocaleLowerCase().includes(searchTerm),
+    );
+  }
+  
+  return res.json(filteredJobs)
+})
+ 
+app.get('/get-single-job/:id', (req, res) => {
+  const { id } = req.params
+
+  return res.json({
+    job: { id, title: `Job with is ${id}` }
+  })
+}) 
+ 
+  
 app.listen(PORT_SERVER, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT_SERVER}`);
 });
