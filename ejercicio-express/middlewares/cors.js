80246bbc42f5567a500297ac0ac483402070de95
antonidev0 <antonibogado0@gmail.com>
2026-06-21 23:06:36 +0200

    agrego cors.js

---
+++ ejercicio-express/middlewares/cors.js
@@ -0,0 +1,44 @@
+import cors from 'cors'
+
+const ACCEPT_ORIGINS = ["http://localhost:5173", "http://localhost:9000"];
+
+// app.use((req, res, next) => {
+//   if (!ACCEPT_ORIGINS.includes(req.headers.origin)) {
+//     console.log("direccion no invitada detectada:");
+//     console.log({
+//       // de donde viene
+//       origen: req.headers.origin,
+
+//       // su direccion IP
+//       ip: req.ip,
+
+//       // que metodo quiso hacer
+//       metodo: req.method,
+
+//       // a donde queria ir
+//       ruta: req.originalUrl,
+
+//       // que navegador usa
+//       navegador: req.headers["user-agent"],
+
+//       fecha: new Date(),
+//     });
+//   }
+//   next();
+// });
+
+export const corsMiddleware = ({ accptedOrigins = ACCEPT_ORIGINS } = {}) => {
+    return cors({
+    // si la direccion eta invitada
+    origin: (origin, callback) => {
+      if (ACCEPT_ORIGINS.includes(origin)) {
+        return callback(null, true);
+        // no hay error, que pase
+      }
+      // Si no dame este error
+      console.log(`Origen: ${origin}`);
+
+      return callback(new Error("Origen no permitido"));
+    },
+  })
+}
