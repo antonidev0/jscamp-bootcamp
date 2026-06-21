123af8c92c0ae7976649c573626b6c2e7dba68f1
antonidev0 <antonibogado0@gmail.com>
2026-06-21 22:58:06 +0200

    agrego app.js

---
+++ ejercicio-express/app.js
@@ -0,0 +1,64 @@
+import express from "express";
+import cors from 'cors'
+
+import jobs from "./jobs.json" with { type: "json" };
+import { DEAFAULTS } from "./config.js";
+
+process.loadEnvFile();
+const PORT_SERVER = process.env.PORT ?? 1234;
+console.log(PORT_SERVER);
+
+
+
+const ACCEPT_ORIGINS = [ 
+  'http://localhost:5173',
+  "http://localhost:9000"
+]
+ 
+app.use((req, res, next) => {
+  if (!ACCEPT_ORIGINS.includes(req.headers.origin)) {
+    console.log("direccion no invitada detectada:");
+    console.log({
+      // de donde viene
+      origen: req.headers.origin,
+
+      // su direccion IP
+      ip: req.ip,
+
+      // que metodo quiso hacer
+      metodo: req.method,
+
+      // a donde queria ir
+      ruta: req.originalUrl,
+
+      // que navegador usa
+      navegador: req.headers["user-agent"], 
+
+      
+      fecha: new Date(),
+    });
+  }
+  next();
+});
+
+app.use(cors({
+  
+  // si la direccion eta invitada
+  origin: (origin, callback) => {
+    if (ACCEPT_ORIGINS.includes(origin)) {
+      return callback(null, true)
+      // no hay error, que pase
+    }
+    // Si no dame este error
+    console.log(`Origen: ${origin}`);
+    
+    return callback(new Error('Origen no permitido'))
+  }
+}));
+
+app.use(express.json())
+
+
+app.listen(PORT_SERVER, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT_SERVER}`);
+});
