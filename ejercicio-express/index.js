c374826f30ae4e1b214dc168dbdb68de97918ace
antonidev0 <antonibogado0@gmail.com>
2026-06-21 19:06:28 +0200

    creo funcion put y patch

--- ejercicio-express/index.js
+++ ejercicio-express/index.js
@@ -83,11 +83,50 @@ app.post("/jobs", (req, res) => {
 
 app.put("/jobs/:id", (req, res) => {
   // reemplazar un job
+
+  // saco el id de la URL
+  const { id } = req.params;
+
+  // busco la posicion del trabajo en el array
+  const index = jobs.findIndex((job) => job.id === id);
+
+  if (index === -1) {
+    // si busco el inidice (el trabajo) y no exite
+    return res.status(404).json({ message: "Trabajo no encontrado" });
+  }
+
+  // saco los campos del body
+  const { titulo, empresa, ubicacion, data } = req.body;
+
+  // armo el objeto nuevo (mantengo el mismo id)
+  const updatedJob = { id, titulo, empresa, ubicacion, data };
+
+  // reemplazo el viejo por el nuevo
+  jobs[index] = updatedJob;
+
+  return res.status(200).json(updatedJob);
 });
 
 app.patch("/jobs/:id", (req, res) => {
   // actualizar un job
-});
+
+  const { id } = req.params;
+
+  // busco la posicion del trabajo en el array
+  const index = jobs.findIndex((job) => job.id === id);
+
+  if (index === -1) {
+    // si no existe
+    return res.status(404).json({ message: "Trabajo no encontrado" });
+  }
+
+  // jobs[index] accede a los inidices de los trabajos
+  // ...jobs[index] copia la lista vieja
+  // , ...req.body y actualiza por esta que te envio, deja el id como esta
+  jobs[index] = { ...jobs[index], ...req.body, id }; 
+
+  return res.status(200).json(jobs[index]);
+}); 
 
 app.delete("/jobs/:id", (req, res) => {
   // Eliminar un job
