e2eedd1ebd32b7df908d5e55756363511137def4
antonidev0 <antonibogado0@gmail.com>
2026-06-22 23:47:16 +0200

    quito los parentesis a los enpoint

--- ejercicio-express/routes/jobs.js
+++ ejercicio-express/routes/jobs.js
@@ -3,15 +3,9 @@ import { JobsControllers } from "../controllers/jobs";
 
 const jobsRouter = Router()
 
-jobsRouter.get("/", JobsControllers.getAll());
-
-jobsRouter.get("/:id", JobsControllers.getId());
-
-// NO ES idenpotente
-jobsRouter.post("/", JobsControllers.create());
-
-jobsRouter.put("/:id", JobsControllers.update());
-
-jobsRouter.patch("/:id", JobsControllers.partialUpdate());
-
-jobsRouter.delete("/:id", JobsControllers.delete());
+jobsRouter.get("/", JobsControllers.getAll);
+jobsRouter.get("/:id", JobsControllers.getId);
+jobsRouter.post("/", JobsControllers.create);
+jobsRouter.put("/:id", JobsControllers.update);
+jobsRouter.patch("/:id", JobsControllers.partialUpdate);
+jobsRouter.delete("/:id", JobsControllers.delete);
