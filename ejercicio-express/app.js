8c2fd5bc37a9c9529bd3fff3e71d478c6f83cc3a
antonidev0 <antonibogado0@gmail.com>
2026-06-25 01:20:19 +0200

    agrego cambios para desplegar en vercel

--- ejercicio-express/app.js
+++ ejercicio-express/app.js
@@ -1,21 +1,24 @@
 import express from "express";
 import { DEAFAULTS } from "./config.js";
 import { logMiddleware, corsMiddleware } from "./middlewares/cors.js";
-import { jobsRouter } from "./routes/jobs.js"
+import { jobsRouter } from "./routes/jobs.js";
 
 import jobs from "./jobs.json" with { type: "json" };
- 
+
 process.loadEnvFile();
 const PORT_SERVER = process.env.PORT ?? 1234;
-const app = express()
-
-app.use(logMiddleware);
+const app = express();
+ 
 app.use(corsMiddleware());
 app.use(express.json());
-app.use("/jobs", jobsRouter); 
+app.use("/jobs", jobsRouter);
 
 console.log(PORT_SERVER);
 
-app.listen(PORT_SERVER, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT_SERVER}`);
-});
+if (process.env.PORT !== "production") {
+  app.listen(PORT_SERVER, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT_SERVER}`);
+  });
+}
+
+export default app;
