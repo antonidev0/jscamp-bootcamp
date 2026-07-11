40c23bbc2671fe0ca7c8b8302abd94cb2c9bccef
antonidev0 <antonibogado0@gmail.com>
2026-07-11 15:10:45 +0200

    agrego avances de E2E

--- 09-testing/tests/example.spec.js
+++ 09-testing/tests/example.spec.js
@@ -1,19 +1,13 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('buscar empleos y aplicar ofertas', async ({ page }) => {
+  // ir a la pagina principal
+  await page.goto("http://localhost:5173/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  const searchInput = page.getByRole('searchbox');
+  await searchInput.fill('React')
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  await page.getByRole('button', { name: 'Buscar' }).click()
+  
+})
