301aa277d647e9d1873b96df0e4081064f9a03e2
antonidev0 <antonibogado0@gmail.com>
2026-07-13 12:57:00 +0200

    agrego avances de E2E

--- 09-testing/tests/example.spec.js
+++ 09-testing/tests/example.spec.js
@@ -11,4 +11,14 @@ test('buscar empleos y aplicar ofertas', async ({ page }) => {
   await page.getByRole('button', { name: 'Buscar' }).click()
   
   const jobCards = page.locator('job-listing-card')
+
+  const firstJobTitle = jobCards.first().getByRole("heading", { level: 3 });
+  await expect(firstJobTitle).toHaveText(/desarrollador/i);
+
+  await page.getByRole("button", { name: "Iniciar sesión" }).click();
+
+  const applyButton = page.getByRole("button", { name: "Aplicar" }).first();
+  await applyButton.click();
+
+  await expect(page.getByRole("button", { name: "Aplicado" })).toBeVisible();
 })
