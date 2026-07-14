574332208ee39d51d6b825aa481f27ad3fb70dfc
antonidev0 <antonibogado0@gmail.com>
2026-07-14 13:47:05 +0200

    agrego avances de E2E ia

--- AI-E2E-TEST/test-ai.js
+++ AI-E2E-TEST/test-ai.js
@@ -1,3 +1,30 @@
+import { test } from 'node:test'
+import assert from 'node:assert';
+
+import { Stagehand } from '@browserbasehq/stagehand';
 process.loadEnvFile();
 
-console.log(process.env.OPENIA_API_KEY);
+test("Un usuario puede entrar a la JSConf y adquirir dos entradas por  €287.98", async () => {
+    const stagehand = new Stagehand({
+      env: "LOCAL",
+      model: "openia/gpt-4o",
+    });
+
+    await stagehand.init()
+    const [page] = stagehand.content.page()
+
+    await page.goto("https://jsconf.es");
+
+      await stagehand.act("Haz clic en el botón de comprar entradas");
+      await stagehand.act("Añade un ticket de entrada general al carrito");
+      await stagehand.act("Añade otro ticket de entrada general al carrito");
+
+      // Extracción de info para validar
+      const subtotal = await stagehand.extract(
+        "Extract the subtotal from the page",
+      );
+
+      assert.equal(subtotal, "287,98");
+
+      await stagehand.close();
+});
