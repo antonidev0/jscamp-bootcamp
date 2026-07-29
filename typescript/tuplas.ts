e9f58b2f816dae7fba2686de2ea1da81dfc6358d
antonidev0 <antonibogado0@gmail.com>
2026-07-29 14:27:17 +0200

    tuplas

---
+++ typescript/tuplas.ts
@@ -0,0 +1,12 @@
+type Personaa = [string, number]
+const persona: Persona = ['midudev', 40]
+const nombre = persona[0] // string
+const edad = persona[1]   // number
+
+
+type Coordenadas = [latitud: number, longitud: number]
+const coordenadas: Coordenadas = [40.7128, -74.0060]
+
+type Rango = [min: number, max: number]
+
+type Persona = [nombre: string, edad: number]
