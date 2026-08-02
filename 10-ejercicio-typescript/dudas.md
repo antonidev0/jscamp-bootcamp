98a231574ddd85e6c1c0a2f3bb129a10e70035e4
antonidev0 <antonibogado0@gmail.com>
2026-08-02 20:07:06 +0200

    avances decimo ejercicio

--- 10-ejercicio-typescript/dudas.md
+++ 10-ejercicio-typescript/dudas.md
@@ -1,4 +1,21 @@
 <!-- Aquí puedes poner tus dudas sobre el ejercicio -->
-No seria mejor usar
-export interface Job {}
+
+para el segundo ejercicio:
+
+No seria mejor usar>
+
+export interface Job {
+  id: string
+  title: string
+  company: string
+  location: string
+  description: string
+  salary?: number // Opcional
+  technologies: Technology[]
+  experienceLevel: ExperienceLevel
+  workMode: WorkMode
+  isActive: boolean
+  postedDate: Date
+}
+
 para los Object, que son objetos complejos? no se si entendi bien esa parte
--- 10-ejercicio-typescript/objects.ts
+++ 10-ejercicio-typescript/objects.ts
@@ -2,14 +2,34 @@
  
 import type { ExperienceLevel, WorkMode, Technology } from './types.ts'
 
-export Job = {
-    id: string;
-    title: string;
-    description: string;
-    company: Company;
-    location: string;
-    salary?: number;
-    experienceLevel: ExperienceLevel;
+export type Job = {
+  id: string
+  title: string
+  company: string
+  location: string
+  description: string
+  salary?: number // Opcional
+  technologies: Technology[]
+  experienceLevel: ExperienceLevel
+  workMode: WorkMode
+  isActive: boolean
+  postedDate: Date
 }
-export type Company = 
-export type Candidate =  
+
+export type Company = {
+    id: string
+  name: string
+  description: string
+  website?: string
+  employees: number
+  foundedYear: number
+}
+export type Candidate = {
+  id: string;
+  name: string;
+  email: string;
+  phone?: string;
+  skills: Technology[];
+  experienceYears: number;
+  resume?: string;
+};
